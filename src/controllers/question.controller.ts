f96e368ee3a61ff10688c29eef8ea877d7cfe246
Luan Chrystian <luanchr14@gmail.com>
2024-07-14 19:20:53 -0300

    create decorator for get user id by token

--- src/controllers/question.controller.ts
+++ src/controllers/question.controller.ts
@@ -1,15 +1,18 @@
 import { Controller, Post, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { CurrentUser } from "src/modules/auth/current-user.decorator";
+import { UserByTokenPayload } from "src/modules/auth/jwt.strategy";
 
 @Controller('/questions')
 @UseGuards(AuthGuard('jwt'))
 export class QuestionController {
 
     @Post('/create')
-    async createQuestion() {
+    async createQuestion(@CurrentUser() user: UserByTokenPayload) {
+    
         return {
-            hello_world : "Hello World",
-            message : "Hello World"
+            message : "Hello World",
+            user_id: user.sub
         }
     }
 }
